2a38fe3edb793f7607620a37c7a77db02d404b14
theYounger <kuyehara1@gmail.com>
2016-06-11 13:12:46 -1000

    context.js: forgot to commit after each question: full  assignment

--- context.js
+++ context.js
@@ -1,13 +1,21 @@
+/*Robot function accesses -this- context to set this.name to argument */
+
 var Robot = function (name) {
   this.name = name;
 
 }
+
+/* add function acceses -this- context to set this.name to this.name or "Humans"
+    return variable + string + (arguments) */
 
 function add ( op1, op2 ) {
   this.name = this.name || "Humans";
   return this.name + " can count to " + (op1 + op2);
 }
 
+/*-------------------------------------------
+------------------VAR SET--------------------
+-------------------------------------------*/
 var voltron = new Robot("Voltron");
 var bender = new Robot("Bender Rodriguez");
 var optimus = new Robot("Optimus Prime");
@@ -17,14 +25,16 @@ var wall_e = new Robot("Wall-E");
 
 
 // #1
-console.log(  );
+console.log(add(0, 1));
 
-// #2  
-console.log(  );
+// #2
+console.log(add.call(voltron, 2, 3));
+//console.log(add(2, 3).call(voltron));
 
 // #3
-console.log(  );
+console.log(add.apply(optimus, [20, 30]));
 
 // #4
 
-console.log(  );
+var calculate = add.bind(bender);
+console.log(calculate("drinking ", "beer"));
